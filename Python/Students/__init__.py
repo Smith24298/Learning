59210145fe8cafe511a2beebc83a3b997d03a3c5
Smith24298 <24bce298@nirmauni.ac.in>
2026-05-25 12:28:08 +0530

    Remove student management features and associated files

--- Python/Students/__init__.py
+++
@@ -1,5 +0,0 @@
-from .add_student import add_student
-from .view_student import view_students
-from .search_student import search_student
-from .delete_students import delete_student
-
--- Python/Students/student.py
+++
@@ -1,4 +0,0 @@
-class Student :
-    def __init__(self):
-        self.name = name
-        self.age = 
